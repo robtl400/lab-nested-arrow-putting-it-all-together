37848440d8d43275c382ebf77e54861690fa5b86
Rob Lord <robertlord@outlook.com>
2025-11-12 20:18:36 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 
+function createLoginTracker (userInfo) {
+  
+  let attemptCount = 0
 
+  const pA = (passwordAttempt) => {
+    attemptCount++
+
+      if (attemptCount <= 3 && passwordAttempt === userInfo.password)
+      return ("Login successful")
+
+        if (attemptCount <= 3 && passwordAttempt !== userInfo.password)
+      return ("Attempt " + attemptCount + ": Login failed")
+
+      else 
+      return ("Account locked due to too many failed login attempts")
+  }
+
+  return pA
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
